e383ff6af8f572efa6f5feb30e92bb089e7b51db
antarthureau <88631510+antarthureau@users.noreply.github.com>
2021-10-17 16:00:10 +0200

    Test code 1
    Initial code commit
    Sensor code
    Relay code
    user input: max and min distance, delay time

---
+++ test_code/test_code.ino
@@ -0,0 +1,48 @@
+const unsigned int TRIG_PIN=13;
+const unsigned int ECHO_PIN=12;
+const unsigned int BAUD_RATE=9600;
+int relayCtrlPin = 8;
+
+int dt = 50; //latence en ms
+int maxDist = 50; //distance maximum en cm
+int minDist = 7;  //distance minimum en cm
+
+
+void setup() {
+  pinMode(TRIG_PIN, OUTPUT);
+  pinMode(ECHO_PIN, INPUT);
+  pinMode(relayCtrlPin, OUTPUT);
+  Serial.begin(BAUD_RATE);
+}
+
+void loop() {
+
+//ultrasonic sensor data in__________________________//
+  digitalWrite(TRIG_PIN, LOW);
+  delayMicroseconds(2);
+  digitalWrite(TRIG_PIN, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(TRIG_PIN, LOW);
+ const unsigned long duration= pulseIn(ECHO_PIN, HIGH);
+ int distance= duration/29/2;
+//__________________________________________________//
+
+if (distance <= maxDist && distance >+ minDist) {
+  digitalWrite(relayCtrlPin, HIGH);
+}
+else {
+  digitalWrite(relayCtrlPin, LOW);
+}
+
+//ultrasonic data serialprint_______________________//
+ if(duration==0){
+   Serial.println("Warning: no pulse from sensor");
+   } 
+  else{
+      Serial.print("distance to nearest object:");
+      Serial.println(distance);
+      Serial.println(" cm");
+  }
+//__________________________________________________//
+ delay(dt);
+ }
